741fdee2caf75fba397e70fd1bc9b1d01773aa55
Nathan985 <nathan.rodrigu3s@gmail.com>
2024-05-10 13:15:33 -0300

    feat: button breadcrumbs and button create

--- app/(user)/command/page.tsx
+++ app/(user)/command/page.tsx
@@ -1,68 +1,70 @@
-import { listCommandsfromComandPage } from "@/app/lib/actions";
-import { Breadcrumbs } from "@/app/ui/breadcrumbs";
-import { Button } from "@/app/ui/button";
-import CommandsList from "@/app/ui/commands-list";
-import EndpointsSearchBar from "@/app/ui/endpoint-search-bar";
-import Link from "next/link";
-import React from "react";
+import { listCommandsfromComandPage } from '@/app/lib/actions';
+import { Breadcrumbs } from '@/app/ui/components/@composition/Breadcrumbs';
+import { Button } from '@/app/ui/components/Button';
+import CommandsList from '@/app/ui/commands-list';
+import EndpointsSearchBar from '@/app/ui/endpoint-search-bar';
+import Link from 'next/link';
+import React from 'react';
 
 const Command: React.FC = async ({
-    searchParams,
+	searchParams,
 }: {
-    searchParams?: {
-        query?: string;
-        type?: string;
-    };
+	searchParams?: {
+		query?: string;
+		type?: string;
+	};
 }) => {
-    const query = searchParams?.query || "";
-    const type = searchParams?.type || undefined;
+	const query = searchParams?.query || '';
+	const type = searchParams?.type || undefined;
 
-    const commandsEntity = await listCommandsfromComandPage(query, type);
+	const commandsEntity = await listCommandsfromComandPage(query, type);
 
-    return (
-        <>
-            <div className="min-h-full">
-                <div className="flex flex-col">
-                    <EndpointsSearchBar
-                        placeholder="Pesquise pelo comando."
-                        fieldsForSearch={[
-                            {
-                                field: "name",
-                                label: "Nome",
-                            },
-                            {
-                                field: "description",
-                                label: "Descrição",
-                            },
-                            {
-                                field: "command",
-                                label: "Comando",
-                            },
-                        ]}
-                    />
-                    <Breadcrumbs
-                        root="/"
-                        data={[
-                            {
-                                href: "/comamand",
-                                name: "Comandos",
-                            },
-                        ]}
-                    />
-                    <div className="flex w-full items-center justify-end px-6">
-                        <Link href={"/command/create"}>
-                            <Button className="bg-indigo-500 hover:bg-indigo-400 transition-colors text-white rounded-md text-sm shadow-md p-2">
-                                Cadastrar Comando
-                            </Button>
-                        </Link>
-                    </div>
-                    <main className="flex-1">
-                        <CommandsList commands={commandsEntity} />
-                    </main>
-                </div>
-            </div>
-        </>
-    );
+	return (
+		<>
+			<div className="min-h-full">
+				<div className="flex flex-col">
+					<EndpointsSearchBar
+						placeholder="Pesquise pelo comando."
+						fieldsForSearch={[
+							{
+								field: 'name',
+								label: 'Nome',
+							},
+							{
+								field: 'description',
+								label: 'Descrição',
+							},
+							{
+								field: 'command',
+								label: 'Comando',
+							},
+						]}
+					/>
+					<div className="flex items-center space-x-4 px-4 py-4 sm:px-6 lg:px-8">
+						<Breadcrumbs
+							homePage={{
+								href: '/',
+							}}
+							pages={[
+								{
+									href: '/comamand',
+									name: 'Comandos',
+								},
+							]}
+						/>
+					</div>
+					<div className="flex w-full items-center justify-end px-6">
+						<Link href={'/command/create'}>
+							<Button>Cadastrar Comando</Button>
+						</Link>
+					</div>
+					<main className="flex-1">
+						<CommandsList commands={commandsEntity} />
+					</main>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Command;
